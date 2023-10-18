2db3112f5dac07c5ada4281cd252922a88eab1b4
ife-chukwu <dopeskiez399@gmail.com>
2023-10-18 05:10:01 +0100

    Updated project section

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./Redux/store.js";
 import { BrowserRouter } from "react-router-dom";
+import "atropos/css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
